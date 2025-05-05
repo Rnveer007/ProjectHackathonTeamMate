b973f562e891076eff2e082cfd9ad1f34fffd37b
Rnveer007 <rvs523474@gmail.com>
2025-05-05 10:41:39 +0530

    form file updated

--- Backend/routes/formRoutes.js
+++ Backend/routes/formRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { submitForm } from '../controller/form.js';
 import {upload} from '../middleware/multer.js'
-import { checkAdminToken } from '../controller/auth.js';
+import {checkAdminToken} from '../controller/adminAuth.js'
 
 const router = express.Router();
 
